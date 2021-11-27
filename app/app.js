be8c47552222ca73cdc51374a9f058b55134bbfd
Sumukha210 <sumukhakb@gmail.com>
2021-11-27 23:16:23 +0530

    useful comments in app.js

--- app/app.js
+++ app/app.js
@@ -1,18 +1,24 @@
+// Grab nav element from the dom
 const nav = document.querySelector("nav");
 
+// Creating variales
 let scroll_position = 30;
 let scroll_direction;
 
+// Show Navbar when dom loads
 nav.classList.add("active__menu");
 
+// Create an scroll event listener
 window.addEventListener("scroll", () => {
   scroll_direction =
     document.body.getBoundingClientRect().top > scroll_position ? "up" : "down";
   scroll_position = document.body.getBoundingClientRect().top;
 
   if (scroll_direction === "up") {
+    // If you are scrolling up, then add active menu class
     nav.classList.add("active__menu");
   } else {
+    // If you Scrolling down, remove the active menu class
     nav.classList.remove("active__menu");
   }
 });
